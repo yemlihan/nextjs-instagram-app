967325537c4e63565c56ba7690743009e05ab3f7
Yemlihan <yemlihansapan1997@gmail.com>
2023-06-02 00:05:37 +0300

    fixed module

--- req-instagram/.next/static/chunks/webpack.js
+++ req-instagram/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5dd4a62c046dce64"; }
+/******/ 		__webpack_require__.h = function() { return "a6d882736b4b4250"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- req-instagram/.next/static/webpack/webpack.a2fd666b4bb9a17d.hot-update.js
+++ req-instagram/.next/static/webpack/webpack.a2fd666b4bb9a17d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a3c16cb25962b17d"; }
+/******/ 	__webpack_require__.h = function() { return "a6d882736b4b4250"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
