a3a9edbd4908b5031064f9f1e405282a92dc89eb
Yemlihan <yemlihansapan1997@gmail.com>
2023-06-28 21:52:56 +0300

    added cors for vercel

--- req-instagram/.next/static/webpack/webpack.919fdc1ed79179b9.hot-update.js
+++ req-instagram/.next/static/webpack/webpack.919fdc1ed79179b9.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "06c16d01ffee9263"; }
+/******/ 	__webpack_require__.h = function() { return "99c7237f1f22ee8b"; }
 /******/ }();
 /******/ 
 /******/ }
--- req-instagram/next.config.js
+++ req-instagram/next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'Access-Control-Allow-Credentials',
+    value: 'true',
+  },
+  {
+    key: 'Access-Control-Allow-Origin',
+    value: '*',
+  },
+  {
+    key: 'Access-Control-Allow-Methods',
+    value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+  },
+  {
+    key: 'Access-Control-Allow-Headers',
+    value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  },
+];
+
 const nextConfig = {
   reactStrictMode: false,
   env: {
@@ -6,6 +25,14 @@ const nextConfig = {
       process.env.NODE_ENV === "production"
         ? process.env.NEXT_PUBLIC_PROD_API_URL
         : process.env.NEXT_PUBLIC_DEV_API_URL,
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
