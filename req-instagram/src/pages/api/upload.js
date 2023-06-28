fa6dfb77ead6303258e54d6f384c73ccd0c12a79
Yemlihan <yemlihansapan1997@gmail.com>
2023-06-28 22:31:47 +0300

    fixed upload js

--- req-instagram/src/pages/api/upload.js
+++ req-instagram/src/pages/api/upload.js
@@ -2,25 +2,27 @@ import { upload } from "./config";
 
 export default function handler(req, res) {
   if (req.method === "POST") {
-    const uploadFiles = upload.array("files");
-    uploadFiles(req, res, (error) => {
-      if (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Dosya yükleme hatası." });
-      }
+    try {
+      const uploadFiles = upload.array("files");
+      uploadFiles(req, res, (error) => {
+        if (error) {
+          console.error(error);
+          return res.status(500).json({ error: "Dosya yükleme hatası: " + error.message });
+        }
 
-      const files = req.files;
-      if (!files || files.length === 0) {
-        return res.status(400).json({ error: "Yüklenen dosya bulunamadı." });
-      }
+        const files = req.files;
+        if (!files || files.length === 0) {
+          return res.status(400).json({ error: "Yüklenen dosya bulunamadı." });
+        }
 
-      res.send({ items: req.files, message: "Successful" });
-    });
+        res.send({ items: req.files, message: "Successful" });
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Beklenmeyen bir hata oluştu." });
+    }
   } else if (req.method === "GET") {
     // Buraya GET işlemleri için gerekli kodları ekleyin (isteğe bağlı)
   }
 }
 
-export const config = {
-  api: { bodyParser: false },
-};
