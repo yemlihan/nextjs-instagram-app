bcad84d6dd9f986302341ae1e72735e247b3020a
Yemlihan <yemlihansapan1997@gmail.com>
2023-06-28 22:01:08 +0300

    fixed url

--- req-instagram/.next/static/chunks/webpack.js
+++ req-instagram/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "99c7237f1f22ee8b"; }
+/******/ 		__webpack_require__.h = function() { return "06c16d01ffee9263"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
